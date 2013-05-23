b06c3ee78e6d17e9a133f99cff1a499945d2bec4
supermomonga <hasegawa@supermomonga.com>
2013-05-24 00:53:00 +0900

    take argv and show

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    if(a.arguments().length() >= 1) {
-        w.setImage("~/Desktop/zimbu.jpg");
+    if(a.arguments().length() > 1) {
+        w.setImage(a.arguments().first();
     }
 
     return a.exec();
