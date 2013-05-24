04125b91a66ac65d6fb6e64557b639d2fb2ad213
supermomonga <hasegawa@supermomonga.com>
2013-05-24 20:27:22 +0900

    url regexp not match...

--- main.cpp
+++ main.cpp
@@ -11,12 +11,18 @@ int main(int argc, char *argv[])
     // Always has atleast one arg which is Application path.
     // So if args is two or more, it passed one or more args from user or another software.
     if(a.arguments().length() > 1) {
-        w.setImage(a.arguments().at(1));
+        w.clearImages();
+        QStringList args = a.arguments();
+        QStringList::const_iterator iter;
+        for (iter = ++args.constBegin(); iter != args.constEnd();++iter){
+            w.setImage(*iter);
+        }
     }
-    w.setImage("/Users/momonga/Desktop/uji.jpg");
-    w.setImage("/Users/momonga/Desktop/zimbu.jpg");
+#define DEBUG
+#ifdef DEBUG
     w.setImage("/Users/momonga/Desktop/uji.jpg");
     w.setImage("https://dl.dropboxusercontent.com/u/330501/Screenshots/vim.png");
+#endif
 
     return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,7 +8,7 @@
 #include <QFileInfo>
 #include <QDir>
 #include <QLabel>
-#include <QMessageBox>
+#include <QRegExp>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -41,17 +41,26 @@ void MainWindow::clearImages(int n)
 void MainWindow::setImage(QString path)
 {
     QLabel *label = new QLabel();
-    QImage image(QFileInfo(path).absoluteFilePath());
-    if(image.isNull()){
-        label->setText("そんな画像ないよ");
-    }else{
-        label->setBackgroundRole(QPalette::Base);
-        QPixmap pm = QPixmap::fromImage(image);
-        label->setPixmap(pm);
-        label->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
-        label->setScaledContents(false);
-        label->adjustSize();
+    QRegExp remoteImage("https?.+(jpg|jpeg|gif|png)");
+    if(remoteImage.indexIn(path) > 0)
+    {
+        QString html = "test:<img src=\"" + path + "\"/>";
+        qDebug() << html;
+        label->setTextFormat(Qt::RichText);
+        label->setText(html);
+    }else {
+        QImage image(QFileInfo(path).absoluteFilePath());
+        if(image.isNull()){
+            label->setText("そんな画像ないよ");
+        }else{
+            label->setBackgroundRole(QPalette::Base);
+            QPixmap pm = QPixmap::fromImage(image);
+            label->setPixmap(pm);
+            label->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+            label->setScaledContents(false);
+            label->adjustSize();
+        }
     }
     ui->verticalLayoutImages->addWidget(label);
-	label->show();
+    label->show();
 }
